0621c2a77f4a8953f1cb78fe815caec7a5b0f32d
Jorge <ardonz@gmail.com>
2019-08-06 08:47:42 -0600

    initial commit on expense-boiler

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routes/AppRouter';
+import './styles/styles.scss';
+
+ReactDOM.render(<AppRouter />, document.querySelector('#root'));
